b41a7b961e0fbde50aecc4847dfb01d0ce84a2b2
chiaros93 <chiara_rossetti@outlook.it>
2023-11-07 15:01:45 +0100

    abrir pagina de registro e acesso en otra pantalla

--- src/components/Botones.jsx
+++ src/components/Botones.jsx
@@ -2,14 +2,14 @@ import './Botones.css'
 
 function Botones() {
     return (
-            <div className='botones'>
-                <button className='registrarte'>
-                    <a id='btn1' href="https://www.spotify.com/es/signup?forward_url=https%3A%2F%2Fopen.spotify.com%2Fintl-es%3F">Registrate</a>
-                </button>
-                <button className='iniciar'>
-                    <a id='btn' href="https://accounts.spotify.com/es-ES/login?continue=https%3A%2F%2Fopen.spotify.com%2Fintl-es%3F">Iniciar Sesión</a>
-                </button>
-            </div>
+        <div className='botones'>
+            <button className='registrarte'>
+                <a id='btn1' href="https://www.spotify.com/es/signup?forward_url=https%3A%2F%2Fopen.spotify.com%2Fintl-es%3F" target="_blank">Registrate</a>
+            </button>
+            <button className='iniciar'>
+                <a id='btn' href="https://accounts.spotify.com/es-ES/login?continue=https%3A%2F%2Fopen.spotify.com%2Fintl-es%3F" target="_blank">Iniciar Sesión</a>
+            </button>
+        </div>
     );
 }
 
