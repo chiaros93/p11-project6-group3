add230bc8e8f27aa760303e41d4a2165eae56db3
Julio <jcsmerubia@gmail.com>
2023-11-08 09:31:24 +0100

    Dom routes

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header"
+import Catalogo from "../components/Catalogo"
+import Botones from "../components/Botones"
+
+
+function Main(){
+    return(
+        <div className='caja1'>
+            <Header />
+            <Catalogo />
+            <Botones />
+        </div>
+    )
+}
+
+
+export default Main;
---
+++ src/pages/SecondPage.jsx
@@ -0,0 +1,9 @@
+function SecondPage(){
+    return(
+        <div>
+            <h1>Hola pisha</h1>
+        </div>
+    );
+}
+
+export default SecondPage;
