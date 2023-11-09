b4540978fd28f141cec2601b182f0f3c0066459b
karol176 <karvascruz@gmail.com>
2023-11-09 09:19:02 +0100

    actualizacion

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -48,7 +48,7 @@ function Search() {
             </button>
             <input type="text" placeholder="buscar" className="br" value={cancion} onChange={e => setCancion(e.target.value)} />
         </form>
-            <div>
+            <div className="ctg">
                 <div className="getAlbums">
                 {canciones.map((cancion, index) => (
                     <>
